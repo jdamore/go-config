e3c668bd7263284644672b66b78b38cf5a3d938b
Jean D'Amore <jdamore@thoughtworks.com>
2012-02-17 20:27:47 +1000

    Added go-agent module

--- app/go-server/go-server.sh
+++ app/go-server/go-server.sh
@@ -2,19 +2,39 @@
 
 CURR_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-function go_server_init() {
+function go_server_config() {
+	echo "GO_SERVER_PORT=$GO_SERVER_IN_PORT" > $CURR_DIR/dist/default.cruise-server
+	echo "export GO_SERVER_PORT" >> $CURR_DIR/dist/default.cruise-server
+	echo "GO_SERVER_SSL_PORT=$GO_SERVER_IN_SSL_PORT" >> $CURR_DIR/dist/default.cruise-server
+	echo "export GO_SERVER_SSL_PORT" >> $CURR_DIR/dist/default.cruise-server
+	echo "SERVER_WORK_DIR=$GO_SERVER_IN_WDIR" >> $CURR_DIR/dist/default.cruise-server
+	echo "export SERVER_WORK_DIR" >> $CURR_DIR/dist/default.cruise-server
+	echo "DAEMON=Y" >> $CURR_DIR/dist/default.cruise-server
+}
+
+function go_server_user() {
 	sudo groupadd go
-	sudo useradd -g go -p /home/go -m go
+	sudo useradd -g go -p /home/go -m go	
+}
+
+function go_server_init() {
+	. $CURR_DIR/go-server.cfg
+	go_server_config
+	go_server_user
 }
 
 function go_server_create() {	
 	sudo cp $CURR_DIR/dist/default.cruise-server /etc/default/go-server	
 	sudo mkdir /usr/share/go-server
+	sudo mkdir /usr/share/go-server/config
 	sudo cp $CURR_DIR/dist/go.jar /usr/share/go-server/.
 	sudo cp $CURR_DIR/dist/server.sh /usr/share/go-server/.
+	sudo cp $CURR_DIR/dist/cruise-comnfig.xml /usr/share/go-server/config/.
 	sudo chown go /usr/share/go-server
 	sudo chown go /usr/share/go-server/*
+	sudo chown go /usr/share/go-server/config/*
 	sudo chmod 755 /usr/share/go-server/*
+	sudo chmod 755 /usr/share/go-server/config/*
 	sudo mkdir /var/lib/go-server
 	sudo chown go /var/lib/go-server
 }
